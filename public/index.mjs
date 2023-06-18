dc32479187cd0a7fd2fcf0937a896f26caf0ce28
oscar welton <oscarwelton@gmail.com>
2023-06-18 13:04:32 +0100

    alot of changes

--- public/index.mjs
+++ public/index.mjs
@@ -1,85 +1,63 @@
-const front = document.querySelector(".front");
-const back = document.querySelector(".back");
-const left = document.querySelector(".left");
-const right = document.querySelector(".right");
-const home = document.querySelector(".home");
-const about = document.querySelector(".about");
-const projects = document.querySelector(".projects");
-const contact = document.querySelector(".contact");
-const box = document.querySelector(".box");
+const surname = document.querySelector(".surname");
+const firstName = document.querySelector(".first-name");
+const about = document.getElementById("about");
+about.addEventListener("click", () => {
+  surname.classList.remove("intro-animation");
+  firstName.classList.remove("intro-animation");
+  firstName.classList.add("up-animation");
+  surname.classList.add("down-animation");
+});
 
-function addClasses() {
-  const boxClasses = `
-  .front {
-    transform: translateZ(${translateZ}px);
-  }
-  .back {
-    transform: translateZ(-${translateZ}px) rotateY(180deg);
-  }
-  .left {
-    transform: translateX(-${translateZ}px) rotateY(-90deg);
-  }
-  .right {
-    transform: translateX(${translateZ}px) rotateY(90deg);
-  }`;
+let y = 0;
 
-  var styleElement = document.createElement('style');
-  styleElement.innerHTML = boxClasses;
-  document.head.appendChild(styleElement);
+function updateCirclePosition(y) {
+  const bg = document.querySelector(".bg");
+  const url = "./resources/grainy.svg";
 
-  front.classList.add("front");
-  back.classList.add("back");
-  left.classList.add("left");
-  right.classList.add("right");
+  const bgString = `radial-gradient(circle at 100% ${y}%, rgba(0, 0, 0, 1), rgba(0, 0, 0, 0)),
+  url('${url}')`;
+  bg.style.background = bgString;
+  // console.log(bgString);
 }
 
-let translateZ;
-translateZ = box.offsetWidth / 2;
-addClasses();
+setInterval(() => {
+  if (y < 150) {
+    y++;
+    y++
+  } else if (y === 150) {
+    y = -150;
+    y++;
+    y++;
+  }
 
-window.addEventListener('resize', function() {
-  translateZ = box.offsetWidth / 2;
-  front.classList.remove("front");
-  back.classList.remove("back");
-  left.classList.remove("left");
-  right.classList.remove("right");
-  addClasses();
-});
+  updateCirclePosition(y);
+}, 100);
 
-home.addEventListener("click", () => {
-  console.log("home clicked");
-  requestAnimationFrame(() => {
-    box.classList.add("rotate-front");
-    box.classList.remove("rotate-back", "rotate-left", "rotate-right");
-  });
-});
+// setInterval(() => {
+//   updateCirclePosition();
+// }, 1000);
 
-about.addEventListener("click", () => {
-  requestAnimationFrame(() => {
-    box.classList.add("rotate-left");
-    box.classList.remove("rotate-back", "rotate-front", "rotate-right");
-  });
-});
+// setInterval(updateCirclePosition, 30000);
+// while (true) {
+//   // Your code logic goes here
 
-projects.addEventListener("click", () => {
-  requestAnimationFrame(() => {
-    box.classList.add("rotate-right");
-    box.classList.remove("rotate-back", "rotate-front", "rotate-left");
-  });
-});
+//   // Check if x reaches 150 or -50 to change direction
+//   if (x === 150 || x === -50) {
+//     direction *= -1; // Change direction
+//   }
 
-contact.addEventListener("click", () => {
-  requestAnimationFrame(() => {
-    box.classList.add("rotate-back");
-    box.classList.remove("rotate-left", "rotate-front", "rotate-right");
-  });
-});
+//   // Check if y reaches 150 or -50 to change direction
+//   if (y === 150 || y === -50) {
+//     direction *= -1; // Change direction
+//   }
 
+//   // Increment x or y based on the current direction
+//   if (x < 150 && x >= -50) {
+//     x += direction;
+//   } else if (y < 150 && y >= -50) {
+//     y += direction;
+//   }
 
-const url = "data:image/svg+xml,%3Csvg viewBox='0 0 400 400' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='9.15' numOctaves='6' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)'/%3E%3C/svg%3E";
-
-const circle = document.getElementById("circle");
-
-circle.style.background = `radial-gradient(circle at 50% 50%, rgba(0,0,0,0.55), rgba(0,0,0,0.2)),
-radial-gradient(circle at 50% 50%, rgba(16,13,13,0.06), rgba(9,10,9,0.19)),
-url(${url})`;
+//   // Print the current values of x and y (you can replace this with your desired logic)
+//   console.log(`x: ${x}, y: ${y}`);
+// }
