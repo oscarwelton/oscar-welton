c280c51a985f70ff0a5a4731cdee82c443f9b12e
oscar welton <oscarwelton@gmail.com>
2023-06-24 19:27:41 +0100

    navbar annimation when selecting different tab

--- public/index.mjs
+++ public/index.mjs
@@ -2,11 +2,6 @@ const bg = document.querySelector(".bg");
 const url = "./resources/grainy.svg";
 
 
-
-
-
-
-
 const surname = document.querySelector(".surname");
 const firstName = document.querySelector(".first-name");
 const about = document.getElementById("about");
@@ -16,9 +11,20 @@ const projectsContent = Array.from(
   document.querySelector(".projects").children
 );
 
+// window.addEventListener('DOMContentLoaded', function() {
+//   var parent = document.querySelector('.bg');
+//   var shadow = document.getElementById('.bg-shadow');
+//   parent.style.width = parent.offsetHeight + 'px';
+//   shadow.style.width = parent.offsetHeight + 'px';
+// });
+
 about.addEventListener("click", () => {
   surname.classList.remove("intro-animation");
   firstName.classList.remove("intro-animation");
+  about.classList.add("active");
+  projects.classList.remove("active");
+  contact.classList.remove("active");
+
 
   setTimeout(() => {
     surname.style.transition = "";
@@ -31,6 +37,9 @@ about.addEventListener("click", () => {
 
 projects.addEventListener("click", () => {
   firstName.classList.remove("intro-animation");
+  projects.classList.add("active");
+  about.classList.remove("active");
+  contact.classList.remove("active")
   surname.classList.remove("intro-animation");
   firstName.classList.add("round-animation");
   surname.classList.add("round-animation");
@@ -47,4 +56,6 @@ projects.addEventListener("click", () => {
 const contactForm = document.querySelector(".contact-container");
 contact.addEventListener("click", () => {
   contactForm.classList.remove("d-none");
+  contact.classList.add("active");
+  projects.classList.remove("active")
 });
