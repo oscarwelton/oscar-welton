33a4c5177dae8959d7cb4eef942861054d17b5bf
oscar welton <oscarwelton@gmail.com>
2023-06-27 18:15:43 +0100

    design: making too many changes still

--- public/index.mjs
+++ public/index.mjs
@@ -6,6 +6,10 @@ const firstName = document.querySelector(".first-name");
 const projectsButton = document.getElementById("projects");
 const projects = document.querySelector(".projects");
 
+const eye = document.querySelector(".eye");
+const pupil = document.querySelector(".pupil");
+const elipse = document.querySelector(".elipse");
+
 projectsButton.addEventListener("click", () => {
   firstName.classList.remove("intro-animation");
   projectsButton.classList.add("active");
@@ -18,20 +22,37 @@ projectsButton.addEventListener("click", () => {
   projects.classList.add("fade");
 });
 
-const contactForm = document.querySelector(".links-div");
+const links = document.querySelector(".links-div");
 
 contact.addEventListener("click", () => {
-  firstName.classList.remove("intro-animation");
-  surname.classList.remove("intro-animation");
+  firstName.classList.remove("fn-animation");
+  surname.classList.remove("ln-animation");
   firstName.classList.add("rotate-left-animation");
   surname.classList.add("rotate-right-animation");
-  contactForm.classList.remove("d-none");
-  contactForm.classList.add("fade");
+
+  elipse.classList.remove("open");
+  elipse.classList.add("links-elipse");
+
+  elipse.style.maxHeight = "100%";
+
+  // document.querySelector(".eye h1").innerHTML = "Let's Connect.";
+  // document.querySelector(".eye h1").style.color = "white";
+
+  const heading = document.querySelector(".eye h1");
+  heading.innerHTML = "Let's Connect.";
+  heading.animate(
+    { color: ["initial", "white"] },
+    { duration: 300, fill: "both", delay: 500 }
+  );
+
+  links.classList.remove("d-none");
+  // links.classList.add("rotate-links");
+
   contact.classList.add("active");
   projectsButton.classList.remove("active");
   projects.classList.add("d-none");
-  bg.classList.add("left");
-  bgShadow.classList.add("left");
+
+  linksFontSize();
 });
 
 const firstNameSpans = Array.from(
@@ -56,31 +77,39 @@ function updateFontSize() {
 updateFontSize();
 window.addEventListener("resize", updateFontSize);
 
-const eye = document.querySelector(".eye");
-const pupil = document.querySelector(".pupil");
-const elipse = document.querySelector(".elipse");
+const contactLinks = Array.from(document.querySelectorAll(".contact-link"));
 
-elipse.addEventListener("animationend", () => {
-  bgShadow.addEventListener("mousemove", function (e) {
-    eye.style.transition = "";
-    pupil.style.transition = "";
+function linksFontSize() {
+  const width = bg.offsetWidth;
+  const linkFontSize = width / 8.5 + "px";
 
-    elipse.classList.remove("open");
-    var x = e.clientX / 10;
-    var y = e.clientY / 10;
-
-    pupil.classList.add("pupil-grow");
-    elipse.style.height = y + "%";
-
-    eye.style.clipPath = `circle(40% at ${x - 5}% ${y + 10}%)`;
-    pupil.style.clipPath = `circle(17% at ${x - 5}% ${y + 10}%)`;
+  contactLinks.forEach((contact) => {
+    contact.style.transition = "font-size 3s";
+    contact.style.fontSize = linkFontSize;
+    contact.style.pointerEvents = "auto";
   });
-});
+}
+
+// elipse.addEventListener("animationend", () => {
+//   bgShadow.addEventListener("mousemove", function (e) {
+//     eye.style.transition = "";
+//     pupil.style.transition = "";
+
+//     elipse.classList.remove("open");
+//     var x = e.clientX / 10;
+//     var y = e.clientY / 10;
+
+//     pupil.classList.add("pupil-grow");
+//     elipse.style.height = y + "%";
+
+//     eye.style.clipPath = `circle(40% at ${x - 5}% ${y + 10}%)`;
+//     pupil.style.clipPath = `circle(17% at ${x - 5}% ${y + 10}%)`;
+//   });
+// });
 
 function blink() {
   firstName.classList.remove("fn-animation");
   firstName.classList.add("blink-top");
-
 
   surname.classList.remove("ln-animation");
   surname.classList.add("blink-bottom");
@@ -89,18 +118,39 @@ function blink() {
   elipse.classList.add("blink");
 }
 
+window.addEventListener("resize", updateCircleDiameter);
 
+function updateCircleDiameter() {
+  var linksCircle = document.getElementById("links-circle");
+  linksCircle.setAttribute("width", bg.offsetWidth);
+  linksCircle.setAttribute("height", bg.offsetWidth);
 
-bgShadow.addEventListener("mouseout", function () {
-  eye.style.transition = "clip-path 0.5s ease";
-  pupil.style.transition = "clip-path 0.5s ease";
-  eye.style.clipPath = `circle(40% at 50% 50%)`;
-  pupil.style.clipPath = `circle(15% at 50% 50%)`;
+  var svg = bgShadow.querySelector("svg");
+  var circlePath = svg.querySelector("#circle-path");
 
-  blink();
-});
+  var parentDivWidth = bgShadow.offsetWidth;
+  var diameter = parentDivWidth - 15;
 
+  var centerX = parentDivWidth / 2;
+  var centerY = parentDivWidth / 2;
 
+  var pathData = `M${centerX} ${centerY} m${-diameter / 2} 0 a${diameter / 2} ${
+    diameter / 2
+  } 0 1 0 ${diameter} 0 a${diameter / 2} ${diameter / 2} 0 1 0 ${-diameter} 0`;
+
+  circlePath.setAttribute("d", pathData);
+}
+
+updateCircleDiameter();
+
+// bgShadow.addEventListener("mouseout", function () {
+//   eye.style.transition = "clip-path 0.5s ease";
+//   pupil.style.transition = "clip-path 0.5s ease";
+//   eye.style.clipPath = `circle(30% at 50% 50%)`;
+//   pupil.style.clipPath = `circle(15% at 50% 50%)`;
+
+//   // blink();
+// });
 
 // const url = "./resources/grainy.svg";
 
