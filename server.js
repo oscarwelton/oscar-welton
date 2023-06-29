1cef185c38d926128c38485f9d784f38b2388586
oscar welton <oscarwelton@gmail.com>
2023-06-29 19:15:05 +0100

    added url redirect middleware

--- server.js
+++ server.js
@@ -3,21 +3,18 @@ import path from "path";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
 import bodyParser from "body-parser";
-import nodemailer from "nodemailer";
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-let transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
+app.use((req, res, next) => {
+  if (req.headers['x-forwarded-proto'] !== 'https') {
+    return res.redirect('https://' + req.headers.host + req.url);
+  }
+  next();
 });
-
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -25,28 +22,6 @@ app.use(bodyParser.json());
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
-});
-
-app.post("/submit", (req, res) => {
-  const { name, subject, email, message } = req.body;
-
-
-  const mailOptions = {
-    from: email,
-    to: process.env.EMAIL_USER,
-    subject: `${subject}`,
-    text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
-  };
-
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      res.json(err);
-    } else {
-      console.log("Email sent: " + info.response);
-      res.json(info);
-      res.send("Success");
-    }
-  });
 });
 
 app.use(express.static(path.join(__dirname, "public")));
